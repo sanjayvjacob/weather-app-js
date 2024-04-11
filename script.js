6711bfe59c442ffde448b810baa354a8b8bc1272
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-04-11 18:08:41 +0100

    weather-app

---
+++ script.js
@@ -0,0 +1,6 @@
+// Weather Form
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector("card");
+const apiKey = "e5b7f18a2850c75347e04b54fc3ac89d";
